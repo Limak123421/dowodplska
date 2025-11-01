6a10d64aaf69c3a409803bdb2c95a82d26f52bfa
PsxScirpter <122206651+Limak123421@users.noreply.github.com>
2025-11-01 21:48:33 +0100

    Update cache.js

--- assets/app/cache.js
+++ assets/app/cache.js
@@ -1,37 +1,35 @@
 var params = new URLSearchParams(window.location.search);
 
 window.onload = async () => {
-    const filesRequest = await fetch('/cache/files');
+    const filesRequest = await fetch('/dowodplska/cache/files');
     const filesResponse = await filesRequest.json();
     
     const files = ['https://unpkg.com/html5-qrcode'];
     const pages = ['card', 'document', 'documents', 'home', 'id', 'more', 'pesel', 'qr', 'scan', 'services', 'shortcuts', 'show'];
 
     pages.forEach((page) => {
-        files.push('/' + page + '?' + params);
-    })
+        files.push('/dowodplska/' + page + '?' + params);
+    });
 
     filesResponse.files.forEach((file) => {
-        files.push('/' + file);
-    })
+        files.push('/dowodplska/' + file);
+    });
 
     const cacheName = 'fobywatel';
-
     const cache = await caches.open(cacheName);
-    await cache.addAll(files)
+    await cache.addAll(files);
 
     const cachedRequests = await cache.keys();
     
     cachedRequests.forEach((request) => {
         checkElement(request, cache);
-    })
+    });
 
-    navigator.serviceWorker.register('/worker.js');
-}
+    navigator.serviceWorker.register('/dowodplska/worker.js');
+};
 
 async function checkElement(request, cache) {
     const cachedResponse = await cache.match(request);
-
     const url = new URL(request.url);
     const modifiedUrl = new URL(url);
 
